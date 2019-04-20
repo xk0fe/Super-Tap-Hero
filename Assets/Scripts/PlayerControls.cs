46110e1aefe9630daa232754da612b8223a31ff4
xk0fe <x.k0fe@yandex.ru>
2019-04-20 16:03:15 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    public Transform groundCheck;
+    //public Transform startPosition;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        rb.velocity = new Vector2(3, rb.velocity.y);
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        animator.SetBool("OnGround", onGround);
+
+        if (Input.GetMouseButtonDown(0) && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+    }
+}
