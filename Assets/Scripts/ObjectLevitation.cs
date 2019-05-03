7049b8a49285b4daf8fee97956181cebeb601750
xk0fe <x.k0fe@yandex.ru>
2019-05-03 22:41:10 +0300

    Create ObjectLevitation.cs
    fancy little script to make main screen game logo look more visually attractive

---
+++ Assets/Scripts/ObjectLevitation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLevitation : MonoBehaviour
+{
+    public float power = .5f;
+
+    void Update()
+    {
+        float reality = power / 2;
+        transform.position = new Vector2 (transform.position.x, transform.position.y + (Mathf.PingPong(Time.time, power) - reality));
+    }
+}
