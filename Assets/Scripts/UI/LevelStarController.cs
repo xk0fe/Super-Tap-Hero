c82411d012b7450ee404c4ebbe977be08a8f36d0
xk0fe <x.k0fe@yandex.ru>
2019-05-05 05:02:58 +0300

    Create LevelStarController.cs

---
+++ Assets/Scripts/UI/LevelStarController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelStarController : MonoBehaviour
+{
+    public LevelManager parent;
+    public string levelName = "L";
+    [Header("Images")]
+    public Sprite SilverStar;
+    public Sprite GoldStar;
+    [Header("1 level")]
+    public GameObject lvl1Star1;
+    public GameObject lvl1Star2;
+    public GameObject lvl1Star3;
+
+    void Update()
+    {
+        ChangeLevelStars();
+    }
+
+    public void ChangeLevelStars()
+    {
+        if (PlayerPrefs.GetInt((levelName + "S1"))  == 1)
+        {
+            lvl1Star1.GetComponent<Image>().sprite = GoldStar;
+        }
+        else
+        {
+            lvl1Star1.GetComponent<Image>().sprite = SilverStar;
+        }
+
+        if (PlayerPrefs.GetInt((levelName + "S2")) == 1)
+        {
+            lvl1Star2.GetComponent<Image>().sprite = GoldStar;
+        }
+        else
+        {
+            lvl1Star2.GetComponent<Image>().sprite = SilverStar;
+        }
+
+        if (PlayerPrefs.GetInt((levelName + "S3")) == 1)
+        {
+            lvl1Star3.GetComponent<Image>().sprite = GoldStar;
+        }
+        else
+        {
+            lvl1Star3.GetComponent<Image>().sprite = SilverStar;
+        }
+    }
+
+}
