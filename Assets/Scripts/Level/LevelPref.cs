64f5d151702956be7f57642350d8a689b12c1bf2
xk0fe <x.k0fe@yandex.ru>
2019-05-05 05:02:46 +0300

    Create LevelPref.cs

---
+++ Assets/Scripts/Level/LevelPref.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPref : MonoBehaviour
+{
+    public string levelName { get; set; }
+    public int star1 { get; set; }
+    public int star2 { get; set; }
+    public int star3 { get; set; }
+    public int locked { get; set; }
+
+    void Level(string levelName, int star1, int star2, int star3, bool locked)
+    {
+        this.levelName = levelName;
+        this.star1 = 0;
+        this.star2 = 0;
+        this.star3 = 0;
+        this.locked = 0;
+    }
+}
