2e38c7bcd3ae3f3c5d3b897c85f5baee52a0c627
xk0fe <x.k0fe@yandex.ru>
2019-05-05 05:02:09 +0300

    Update MenuUI.cs

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -1,56 +1,15 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
-    public LevelManager parent;
     public Text AllCoinCountText;
-    [Header("Images")]
-    public Sprite SilverStar;
-    public Sprite GoldStar;
-    [Header("1 level")]
-    public GameObject lvl1Star1;
-    public GameObject lvl1Star2;
-    public GameObject lvl1Star3;
 
-    void Update()
+    private void Update()
     {
         AllCoinCountText.text = LevelManager.CoinCount.ToString();
-        DataManager.LoadData();
-        ChangeLevelStars();
     }
-
-    void ChangeLevelStars()
-    {
-        if (DataManager.Level1Star1 == 1)
-        {
-            lvl1Star1.GetComponent<Image>().sprite = GoldStar;
-        }
-        else
-        {
-            lvl1Star1.GetComponent<Image>().sprite = SilverStar;
-        }
-
-        if (DataManager.Level1Star2 == 1)
-        {
-            lvl1Star2.GetComponent<Image>().sprite = GoldStar;
-        }
-        else
-        {
-            lvl1Star2.GetComponent<Image>().sprite = SilverStar;
-        }
-
-        if (DataManager.Level1Star3 == 1)
-        {
-            lvl1Star3.GetComponent<Image>().sprite = GoldStar;
-        }
-        else
-        {
-            lvl1Star3.GetComponent<Image>().sprite = SilverStar;
-        }
-    }
-
+    
 }
